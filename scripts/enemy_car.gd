63c0d8e492d362c5595964d0379b426052ae5509
Matyáš Trnka <hello@matrn.dev>
2026-09-16 23:40:14 +0200

    minor bugfixes

--- scripts/enemy_car.gd
+++ scripts/enemy_car.gd
@@ -3,13 +3,14 @@ extends CharacterBody2D
 signal passed
 signal crashed
 
+var speed = randi_range(250, 900)
+
 func _ready() -> void:
 	var random = randi_range(1, 138)
 	$Sprite2D.texture = load("res://assets/TopDown Vehicles/other_cars/" + str(random) + ".png")
 
 func _physics_process(delta: float) -> void:
-	var random = randi_range(250, 900)
-	position.y += delta * random
+	position.y += delta * speed
 	if position.y > 700:
 		passed.emit()
 		queue_free()
@@ -18,3 +19,5 @@ func _physics_process(delta: float) -> void:
 func _on_hitbox_body_entered(body: Node2D) -> void:
 	if body.name == "PlayerCar":
 		crashed.emit()
+	if body.name.begins_with("EnemyCar") and not body == self:
+		queue_free()
